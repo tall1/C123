1c2440e514e193faddec8ad666e96064144d0487
tall1 <talev142@gmail.com>
2021-04-19 15:56:11 +0300

    Moved test classes to tst package

--- src/main/java/com/c123/mongoDB/InitDB.java
+++ src/main/java/com/c123/mongoDB/InitDB.java
@@ -10,30 +10,17 @@ public class InitDB {
     public static void main(String[] args) {
         // Creating a Mongo client
         MongoClient mongo = new MongoClient("localhost", 27017);
-
-        // Creating Credentials
-/*
-        MongoCredential credential;
-        credential =
-                MongoCredential.createCredential(
-                        "sampleUser",
-                        "myDb",
-                        "password".toCharArray());
-*/
         System.out.println("Connected to the database successfully");
 
         // Accessing the database
         MongoDatabase database = mongo.getDatabase("myDb");
-/*
-        System.out.println("Credentials ::" + credential);
-*/
 
         //Creating a collection
         database.createCollection("sampleCollection");
         System.out.println("Collection created successfully");
 
-        // Retrieving a collection
-        MongoCollection<Document> collection = database.getCollection("sampleCollection");
+        /*// Retrieving a collection
+        MongoCollection<Document> collection = database.getCollection("sampleCollection");*/
 
         System.out.println("Collection sampleCollection selected successfully");
     }
